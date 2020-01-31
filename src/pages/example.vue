17ec3ad4d76b0fdcf9f11adaf7eb8bcfca4a028c
QinXuYang <1446445040@qq.com>
2020-01-31 14:00:47 +0800

    图片替换为在线图片，减小项目体积

--- src/pages/example.vue
+++ src/pages/example.vue
@@ -16,12 +16,13 @@ export default {
   },
   data () {
     return {
+      path: 'http://file.biubiubius.com/background',
       images: []
     }
   },
   methods: {
     onChange (index, oldIndex) {
-      console.log(index, oldIndex)
+      console.log(`curIndex：${index}`, `oldIndex：${oldIndex}`)
     }
   },
   created () {
@@ -29,7 +30,7 @@ export default {
       '古风男子.jpg', '对弈.jpeg', '我的一个道姑朋友.png', '断桥.png',
       '昨日青空.jpg', '李白.jpg', '独占我的英雄.jpg', '花丛.jpg', '魔道祖师.jpg'
     ]
-    this.images = img.map(item => require(`@/assets/images/${item}`))
+    this.images = img.map(item => `${this.path}/${item}`)
   }
 }
 </script>
